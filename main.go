9c02315e5e82e81b91bdf12fcde113fb1ec12082
Ella-Kadriaieva <ella.kadriaieva@gmail.com>
2025-04-19 15:27:35 +0300

    change port

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"log"
-
-	"github.com/gin-gonic/gin"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
-	db "github.com/troodinc/trood-front-hackathon/database"
-	_ "github.com/troodinc/trood-front-hackathon/docs"
-	"github.com/troodinc/trood-front-hackathon/handlers"
-	"github.com/gin-contrib/cors"
-	"time"
+    "log"
+    "os"
+    "github.com/gin-gonic/gin"
+    swaggerFiles "github.com/swaggo/files"
+    ginSwagger "github.com/swaggo/gin-swagger"
+    db "github.com/troodinc/trood-front-hackathon/database"
+    _ "github.com/troodinc/trood-front-hackathon/docs"
+    "github.com/troodinc/trood-front-hackathon/handlers"
+    "github.com/gin-contrib/cors"
+    "time"
 )
 
 // @title Trood Front Hackathon API
@@ -20,36 +20,40 @@ import (
 // @BasePath /
 
 func main() {
-	db.InitDatabase()
-	handlers.InitProjects()
+    db.InitDatabase()
+    handlers.InitProjects()
 
-	r := gin.Default()
-	
-r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+    r := gin.Default()
+    
+    r.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"http://localhost:5173"},
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+        MaxAge:           12 * time.Hour,
+    }))
+    r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	r.GET("/projects", handlers.GetProjects)
-	r.GET("/projects/:id", handlers.GetProjectByID)
-	r.POST("/projects", handlers.CreateProject)
-	r.PUT("/projects/:id", handlers.EditProject)
-	r.DELETE("/projects/:id", handlers.DeleteProject)
+    r.GET("/projects", handlers.GetProjects)
+    r.GET("/projects/:id", handlers.GetProjectByID)
+    r.POST("/projects", handlers.CreateProject)
+    r.PUT("/projects/:id", handlers.EditProject)
+    r.DELETE("/projects/:id", handlers.DeleteProject)
 
-	r.GET("/projects/:id/vacancies", handlers.GetVacancies)
-	r.POST("/projects/:id/vacancies", handlers.CreateVacancy)
-	r.PUT("/vacancies/:id", handlers.EditVacancy)
-	r.DELETE("/vacancies/:id", handlers.DeleteVacancy)
+    r.GET("/projects/:id/vacancies", handlers.GetVacancies)
+    r.POST("/projects/:id/vacancies", handlers.CreateVacancy)
+    r.PUT("/vacancies/:id", handlers.EditVacancy)
+    r.DELETE("/vacancies/:id", handlers.DeleteVacancy)
 
-	port := "8080"
-	log.Println("Server running on http://localhost:" + port)
+    // Use the PORT environment variable or default to 8080 for local development
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080" // Default to 8080 for local development
+    }
 
-	if err := r.Run(":" + port); err != nil {
-		log.Fatalf("Server failed to start: %v", err)
-	}
+    log.Println("Server running on port " + port)
+    if err := r.Run(":" + port); err != nil {
+        log.Fatalf("Server failed to start: %v", err)
+    }
 }
