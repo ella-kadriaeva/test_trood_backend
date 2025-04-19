c51798f0242dd665785c3ad9fecbd8cd27709745
Ella-Kadriaieva <ella.kadriaieva@gmail.com>
2025-04-19 18:47:50 +0300

    add front link

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
     r := gin.Default()
     
     r.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"http://localhost:5173"},
+        AllowOrigins:     []string{"http://localhost:5173", "http://projectvac.xyz"},
         AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
         AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
         ExposeHeaders:    []string{"Content-Length"},
